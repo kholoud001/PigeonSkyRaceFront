64ae06773f947d1afb2b5d4973b5333db5a3b9e4
kholoud001 <kholoud.sanak@gmail.com>
2025-01-16 22:36:58 +0100

    pigeon crud by breeder

--- src/app/modules/user/pigeon/update-pigeon/update-pigeon.component.ts
+++ src/app/modules/user/pigeon/update-pigeon/update-pigeon.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UserApiService } from '../../../../services/api/user/user-api.service';
-import { Pigeon } from '../../../../services/api/user/user-api.service';
-import {FormsModule} from '@angular/forms';
+import { UserApiService, Pigeon } from '../../../../services/api/user/user-api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-pigeon',
   templateUrl: './update-pigeon.component.html',
   standalone: true,
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   styleUrls: ['./update-pigeon.component.css']
 })
 export class UpdatePigeonComponent implements OnInit {
-  pigeon: Pigeon | undefined;
+  pigeon: Pigeon = {
+    id: null,
+    ringNumber: '',
+    age: null,
+    color: '',
+    gender: ''
+  };
+  isLoading = true;
+  hasError = false;
 
   constructor(
     private userApiService: UserApiService,
@@ -24,32 +29,43 @@ export class UpdatePigeonComponent implements OnInit {
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (isNaN(id)) {
+      alert('Invalid pigeon ID');
+      this.router.navigate(['/user/pigeons']);
+      return;
+    }
+
     this.userApiService.getPigeonById(id).subscribe(
       (data) => {
         this.pigeon = data;
+        this.isLoading = false;
       },
       (error) => {
         console.error('Error fetching pigeon data:', error);
+        this.hasError = true;
+        this.isLoading = false;
         alert('An error occurred while loading the pigeon details.');
+        this.router.navigate(['/user/pigeons']);
       }
     );
   }
 
-  // updatePigeon(): void {
-  //   if (this.pigeon) {
-  //     const id = this.pigeon.id;
-  //     this.userApiService.updatePigeon(id, this.pigeon).subscribe(
-  //       () => {
-  //         alert('Pigeon updated successfully!');
-  //         this.router.navigate(['/user/pigeons']);
-  //       },
-  //       (error) => {
-  //         console.error('Error updating pigeon:', error);
-  //         alert('An error occurred while updating the pigeon.');
-  //       }
-  //     );
-  //   }
-  // }
+  updatePigeon(): void {
+    if (this.pigeon && this.pigeon.id != null) {
+      this.userApiService.updatePigeon(this.pigeon.id, this.pigeon).subscribe(
+        () => {
+          alert('Pigeon updated successfully!');
+          this.router.navigate(['/user/pigeons']);
+        },
+        (error) => {
+          console.error('Error updating pigeon:', error);
+          alert('An error occurred while updating the pigeon.');
+        }
+      );
+    } else {
+      alert('Pigeon data is invalid.');
+    }
+  }
 
   goBack(): void {
     this.router.navigate(['/user/pigeons']);
