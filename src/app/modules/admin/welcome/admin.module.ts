0721631675913a7dbd1d1afaf84ec83ddd066744
kholoud001 <kholoud.sanak@gmail.com>
2025-01-17 16:55:30 +0100

    PF-17 language support

--- src/app/modules/admin/welcome/admin.module.ts
+++ src/app/modules/admin/welcome/admin.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from '../admin-routing.module';
 import { AdminComponent } from './admin.component';
+import {TranslatePipe} from "@ngx-translate/core";
 
 
 @NgModule({
   declarations: [
     AdminComponent
   ],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        AdminRoutingModule,
+        TranslatePipe
+    ]
 })
 export class AdminModule { }
